a5f3304d99d584706795accf802d3b40a3627073
Akkubora <baran_bora@web.de>
2025-06-08 15:23:06 +0200

    .

--- src/reproduction_pipelines.py
+++ src/reproduction_pipelines.py
@@ -26,7 +26,7 @@ MEASUREMENTS_SWEEP2 = 25   # for SWEEP 2 and SWEEP 3
 SHOTS               = 1
 STEPS               = 8
 
-STEPS_SWEEP1               = 8
+STEPS_SWEEP1               = 12
 
 # Sweep lists
 SPREADINGS_SWEEP1 = [1, 2, 4, 8, 16, 32, 64, 128]
