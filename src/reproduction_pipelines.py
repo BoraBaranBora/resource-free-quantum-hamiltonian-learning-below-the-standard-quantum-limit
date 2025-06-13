a1d8ac6c153dee33f9089c4445202c02c532f920
Akkubora <baran_bora@web.de>
2025-06-13 20:37:38 +0200

    for Ham values of O(1) the error should go to 1e-2, therefore shots to two, to brach noise floor

--- src/reproduction_pipelines.py
+++ src/reproduction_pipelines.py
@@ -23,10 +23,10 @@ DEMO    = os.path.join(DEMOPATH, "learn_hamiltonian.py")
 # Shared fixed settings
 MEASUREMENTS_SWEEP1 = 25   # for SWEEP 1
 MEASUREMENTS_SWEEP2 = 25   # for SWEEP 2 and SWEEP 3
-SHOTS               = 1
+SHOTS               = 2
 STEPS               = 8
 
-STEPS_SWEEP1        = 15
+STEPS_SWEEP1        = 14
 
 # Sweep lists
 SPREADINGS_SWEEP1 = [1, 2, 4, 8, 16, 32, 64, 128]
