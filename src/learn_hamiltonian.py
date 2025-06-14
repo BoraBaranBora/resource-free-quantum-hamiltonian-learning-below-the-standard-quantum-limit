b3598dc554cbdfe8c2b102db7bc9c7e51a15b8c8
Akkubora <baran_bora@web.de>
2025-06-14 20:45:45 +0200

    .

--- src/learn_hamiltonian.py
+++ src/learn_hamiltonian.py
@@ -85,7 +85,7 @@ def get_max_batch_size(nq, overhead_gb=2.5, round_to=50, safety=0.9):
     if round_to and round_to > 1:
         bs = (bs // round_to) * round_to
         
-    print(f'batchsize :{max(bs, 1)}')
+    #print(f'batchsize :{max(bs, 1)}')
     return max(bs, 1)
 
 
