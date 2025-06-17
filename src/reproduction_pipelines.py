e0a20be317de3663440decf364f5fbb81e67aa3b
Akkubora <baran_bora@web.de>
2025-06-18 00:01:35 +0200

    abort and abort previous run

--- src/reproduction_pipelines.py
+++ src/reproduction_pipelines.py
@@ -23,14 +23,14 @@ DEMO    = os.path.join(DEMOPATH, "learn_hamiltonian.py")
 # Shared fixed settings
 MEASUREMENTS_SWEEP1 = 25   # for SWEEP 1
 MEASUREMENTS_SWEEP2 = 25   # for SWEEP 2 and SWEEP 3
-SHOTS               = 1
+SHOTS               = 2
 STEPS               = 8
 
 STEPS_SWEEP1        = 15
 
 # Sweep lists
 SPREADINGS_SWEEP1 = [1, 2, 4, 8, 16, 32, 64, 128]
-SPREADINGS_SWEEP3 = [1, 16, 32, 64, 128, 264, 528]#[1, 10, 25, 50, 100, 250, 500]# #
+SPREADINGS_SWEEP3 = [264, 528]#[1, 16, 32, 64, 128, 264, 528]#[1, 10, 25, 50, 100, 250, 500]# #
 ALPHAS     = [1.0,0.9,0.8,0.7,0.6,0.5,0.4,0.3]
 
 
